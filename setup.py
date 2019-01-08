d084b2da6cc2e237533ac9ba346a1233c6db8989
Davide Palmisano <davide@asidatascience.com>
2019-01-08 17:39:56 +0000

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = '''
+Auxein is a Python framework for evolutionary algorithms.
+'''
+
+setup(name='Auxein',
+      version='0.0.1',
+      description='Evolutionary algorithms for everyone.',
+      long_description=long_description,
+      author='Davide Palmisano',
+      author_email='dpalmisano@gmail.com',
+      url='https://github.com/auxein/auxein',
+      download_url='https://github.com/auxein/auxein/releases',
+      license='Apache 2',
+      install_requires=['numpy>=1.15.4'],
+      packages=find_packages()
+)
