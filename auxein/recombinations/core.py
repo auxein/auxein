0ccfad0affbaa2a791c3d37a69c1373b8f717339
Davide Palmisano <dpalmisano@asidatascience.com>
2019-04-18 11:05:50 +0100

    Test uneven recombination.

--- auxein/recombinations/core.py
+++ auxein/recombinations/core.py
@@ -12,6 +12,9 @@ import numpy as np
 
 class Recombination(ABC):
 
+    def __init__(self, allow_uneven: bool = False):
+        self.allow_uneven = allow_uneven
+
     @abstractmethod
     def recombine(self, parent1_dna: np.ndarray, parent2_dna: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         pass
@@ -19,8 +22,8 @@ class Recombination(ABC):
 
 class SimpleArithmetic(Recombination):
 
-    def __init__(self, alpha: float) -> None:
-        super().__init__()
+    def __init__(self, alpha: float, allow_uneven: bool = False) -> None:
+        super().__init__(allow_uneven=allow_uneven)
         self.alpha = alpha
 
     def __linear_combination(self, arr1: np.ndarray, arr2: np.ndarray) -> List[float]:
@@ -30,7 +33,10 @@ class SimpleArithmetic(Recombination):
         return result
 
     def recombine(self, parent1_dna: np.ndarray, parent2_dna: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        cross_over_point = np.random.randint(0, len(parent1_dna))
+        if not self.allow_uneven:
+            assert len(parent1_dna) == len(parent2_dna), 'dna of parents must be of the same dimension'
+    
+        cross_over_point = np.random.randint(0, min(len(parent1_dna), len(parent2_dna)))
         child1_dna = np.concatenate(
             (
                 parent1_dna[:cross_over_point],
@@ -40,6 +46,8 @@ class SimpleArithmetic(Recombination):
                 )
             )
         )
+        if self.allow_uneven and len(child1_dna) < len(parent1_dna):
+            child1_dna = np.concatenate((child1_dna, parent1_dna[len(child1_dna):]))
 
         child2_dna = np.concatenate(
             (
@@ -50,4 +58,7 @@ class SimpleArithmetic(Recombination):
                 )
             )
         )
+        if self.allow_uneven and len(child2_dna) < len(parent2_dna):
+            child2_dna = np.concatenate((child2_dna, parent2_dna[len(child2_dna):]))
+
         return (child1_dna, child2_dna)
